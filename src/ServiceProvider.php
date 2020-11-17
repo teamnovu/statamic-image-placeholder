9e8c4c14b68f8eb7c4028a298c137b33b8e35ff6
Timo <Timo@MacBook-Pro-2.local>
2020-11-17 16:32:39 +0100

    fixes bug where resource binding would fail

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Teamnovu\StatamicImagePlaceholder;
 
-use App\Http\Resources\CustomEntryResource;
+use Teamnovu\Http\Resources\CustomEntryResource;
 use Statamic\Http\Resources\API\EntryResource;
 use Statamic\Http\Resources\API\Resource;
 use Statamic\Providers\AddonServiceProvider;
